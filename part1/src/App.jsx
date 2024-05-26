c4457d510cf71f458567c0acbb6d2bae88db6de8
Samanta Bordallo <bordallo.samanta@gmail.com>
2024-05-26 12:07:02 +0100

    Exercise 1.1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,37 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-
-const now = new Date()
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total total = {exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
+
+const Header = (props) => {
+  return (
     <>
-    <p>
-      Samanta
-    </p>
-    <p>
-    {now.toString()}
-    </p>
-    <Hello name="Hello Victor">
-
-    </Hello>
-    <Hello>
-      
-    </Hello>
-
+    <h1>{props.course}</h1>
     </>
   )
 }
 
-const Hello = (props) => {
-  console.log(props)
+const Content = (props) => {
   return (
-<p>
-  {props.name}
-</p>
+    <>
+    <p>{props.part1} {props.exercises1} </p>
+    <p>{props.part2} {props.exercises2} </p>
+    <p>{props.part3} {props.exercises3} </p>
+    </>
+
   )
 }
-export default App
+
+const Total = (props) => {
+  return (
+    <>
+    <p>Number of exercises {props.total}</p>
+    </>
+  )
+}
