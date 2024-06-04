170229f23c38387be8796118f0e1d1ae209d1939
Samanta Bordallo <bordallo.samanta@gmail.com>
2024-06-04 19:00:28 +0100

    feat: course information step 3 (exercise 1.3)

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,56 +1,72 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total total = {exercises1 + exercises2 + exercises3} />
-    </div>
-  )
-}
-
-export default App
-
-const Header = (props) => { //the props is a object
-  return (
-    <>
-    <h1>{props.course}</h1>
-    </>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <>
-    <Part name = {props.part1} exercises={props.exercises1} />
-    <Part name = {props.part2} exercises={props.exercises2} />
-    <Part name = {props.part3} exercises={props.exercises3} />
-    </>
-
-  )
-}
-
-const Total = (props) => {
-  return (
-    <>
-    <p>Number of exercises {props.total}</p>
-    </>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-    <p>
-      {props.name} {props.exercises}
-    </p>
-    </>
-  )
-}
+    const part1 = {
+      name: 'Fundamentals of React',
+      exercises: 10
+    }
+    const part2 = {
+      name: 'Using props to pass data',
+      exercises: 7
+    }
+    const part3 = {
+      name: 'State of a component',
+      exercises: 14
+    }
+  
+  
+    return (
+      <div>
+        <Header course={course} />
+  
+  
+  
+        <Content 
+        part1={part1.name} exercises1={part1.exercises}
+        part2={part2.name} exercises2={part2.exercises} 
+        part3={part3.name} exercises3={part3.exercises} 
+  />
+        <Total total = {part1.exercises + part2.exercises + part3.exercises} />
+      </div>
+    )
+  }
+  
+  export default App
+  
+  const Header = (props) => { //the props is a object
+    return (
+      <>
+      <h1>{props.course}</h1>
+      </>
+    )
+  }
+  
+  const Content = (props) => {
+    return (
+      <>
+      <Part name = {props.part1} exercises={props.exercises1} />
+      <Part name = {props.part2} exercises={props.exercises2} />
+      <Part name = {props.part3} exercises={props.exercises3} />
+      </>
+  
+    )
+  }
+  
+  const Total = (props) => {
+    return (
+      <>
+      <p>Number of exercises: {props.total}</p>
+      </>
+    )
+  }
+  
+  const Part = (props) => {
+    return (
+      <>
+      <p>
+        {props.name} {props.exercises}
+      </p>
+      </>
+    )
+  }
+  
+  
