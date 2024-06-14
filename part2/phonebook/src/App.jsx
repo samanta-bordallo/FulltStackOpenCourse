d6a7bc2cb1b57f1dc933745a8e518b589d3a631d
Samanta Bordallo <bordallo.samanta@gmail.com>
2024-06-14 11:42:26 +0100

    feat: the phonebook step 5 (exercise 2.10)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,10 +4,16 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: '', number: '' }
   ])
+
+  const [searchName, setSearchName] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [searchName, setSearchName] = useState('')
-
+  const onNewNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const onNewNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
 
   const addName = (event) => {
     event.preventDefault()
@@ -23,64 +29,73 @@ const App = () => {
 
       setPersons(persons.concat(newNameObj))
     }
-
     setNewName('')
     setNewNumber('')
   }
 
-  const onNewNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const onNewNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
   const filteredPersons = persons.filter(person =>
     person.name.toLowerCase().includes(searchName.toLowerCase())
   )
 
-
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter searchName={searchName} setSearchName={setSearchName} />
+      <h3>add a new</h3>
+      <PersonForm newName={newName} newNumber={newNumber} addName={addName} onNewNameChange={onNewNameChange} onNewNumberChange={onNewNumberChange} />
+      <h3>Numbers</h3>
+      <Persons filteredPersons={filteredPersons} />
+    </div>
+  )
+}
+
+const Persons = ({ filteredPersons }) => {
+  return (
+    <ul>
+      {filteredPersons.map((person, index) =>
+        <li key={index}>
+          {person.name} {person.number}
+        </li>
+      )}
+    </ul>
+  )
+}
+
+const Filter = ({ searchName, setSearchName }) => {
+  return (
+    <div>
+      filter shown with
+      <input
+        type="text"
+        value={searchName}
+        onChange={(event) => setSearchName(event.target.value)}
+        placeholder="Search by name"
+      />
+    </div>
+  )
+}
+
+const PersonForm = ({ newName, newNumber, addName, onNewNameChange, onNewNumberChange }) => {
+  return (
+    <form onSubmit={addName}>
       <div>
-        filter shown with
+        name:
         <input
-          typy="text"
-          value={searchName}
-          onChange={(event) => setSearchName(event.target.value)}
-          placeholder="Search by name"
+          value={newName}
+          onChange={onNewNameChange}
         />
       </div>
-
-      <h2>add a new</h2>
-      <form onSubmit={addName}>
-        <div>
-          name:
-          <input
-            value={newName}
-            onChange={onNewNameChange}
-          />
-        </div>
-        <div>
-          number:
-          <input
-            value={newNumber}
-            onChange={onNewNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {filteredPersons.map((person, index) =>
-          <li key={index}>
-            {person.name} {person.number}
-          </li>
-        )}
-      </ul>
-    </div>
+      <div>
+        number:
+        <input
+          value={newNumber}
+          onChange={onNewNumberChange}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
   )
 }
 
